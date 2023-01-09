0116c911f70a5675e2096a92991842f18373efa8
MV1337 <matheusvinicius206@gmail.com>
2023-01-09 16:58:36 -0300

    Testes para listagem de carro disponiveis

---
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.spec.ts
@@ -0,0 +1,82 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { ListAvailableCarsUseCase } from "./ListAvailableCarsUseCase";
+
+let listAvailableCarsUseCase: ListAvailableCarsUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("List cars", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    listAvailableCarsUseCase = new ListAvailableCarsUseCase(carsRepositoryInMemory);
+  });
+
+  it("should be able to list all available cars", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car 1",
+      description: "Car Description",
+      daily_rate: 200,
+      license_plate: "VGG-3224",
+      fine_amount: 150,
+      brand: "Car_Brand",
+      category_id: "Category_id",
+    });
+
+    const cars = await listAvailableCarsUseCase.execute({});
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able to list all available cars by brand", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car2",
+      description: "Car Description",
+      daily_rate: 200,
+      license_plate: "VGG-3224",
+      fine_amount: 150,
+      brand: "Car_Brand_Test",
+      category_id: "Category_id",
+    });
+
+    const cars = await listAvailableCarsUseCase.execute({
+      brand: "Car_Brand_Test",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able to list all available cars by name", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car3",
+      description: "Car Description",
+      daily_rate: 200,
+      license_plate: "POO-123",
+      fine_amount: 150,
+      brand: "Car_Brand_Test",
+      category_id: "Category_id",
+    });
+
+    const cars = await listAvailableCarsUseCase.execute({
+      name: "Car3",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("should be able to list all available cars by category", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car4",
+      description: "Car Description",
+      daily_rate: 200,
+      license_plate: "POO-123",
+      fine_amount: 150,
+      brand: "Car_Brand_Test",
+      category_id: "12345",
+    });
+
+    const cars = await listAvailableCarsUseCase.execute({
+      category_id: "12345",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+});
---
+++ src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -0,0 +1,28 @@
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { inject, injectable } from "tsyringe";
+
+interface Irequest {
+  category_id?: string;
+  brand?: string;
+  name?: string;
+}
+
+@injectable()
+class ListAvailableCarsUseCase {
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) {}
+
+  async execute({ brand, category_id, name }: Irequest): Promise<Car[]> {
+    const cars = await this.carsRepository.findAvailable(
+      brand,
+      category_id,
+      name
+    );
+    return cars;
+  }
+}
+
+export { ListAvailableCarsUseCase };
