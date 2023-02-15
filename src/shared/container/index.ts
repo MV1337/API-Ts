2e08185af2933ad187a31f2eff08414568b92909
MV1337 <matheusvinicius206@gmail.com>
2023-02-15 17:02:52 -0300

    Finalizando parte de rental

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,5 @@
 import { container } from "tsyringe";
+import "@shared/container/providers"
 import { UsersRepository } from "@modules/accounts/infra/typeorm/respositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
@@ -9,6 +10,8 @@ import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositorios/CarsRepository";
 import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositorios/CarsImagesRepository";
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { RentalsRepository } from "@modules/rentals/infra/typeorm/repositories/RentalsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -25,12 +28,14 @@ container.registerSingleton<IUsersRepository>(
   UsersRepository
 );
 
-container.register<ICarsRepository> (
-  "CarsRepository",
-  CarsRepository
-)
+container.register<ICarsRepository>("CarsRepository", CarsRepository);
 
-container.register<ICarsImagesRepository> (
+container.register<ICarsImagesRepository>(
   "CarsImagesRepository",
   CarsImagesRepository
-)
+);
+
+container.registerSingleton<IRentalsRepository>(
+  "RentalsRepository",
+  RentalsRepository
+);
