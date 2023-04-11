d15f67aa099a818f9625120d990bdfa224ed08a2
MV1337 <matheusvinicius206@gmail.com>
2023-04-11 16:02:49 -0300

    Create template for forgot password

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,6 +1,8 @@
 import { injectable } from "tsyringe";
 import { IMailProvider } from "../IMailProvider";
 import nodemailer, { Transporter } from "nodemailer";
+import fs from "fs";
+import handlebars from "handlebars";
 
 @injectable()
 class EtherealMailProvider implements IMailProvider {
@@ -24,13 +26,23 @@ class EtherealMailProvider implements IMailProvider {
       })
       .catch((err) => console.log(err));
   }
-  async sendMail(to: string, subject: string, body: string): Promise<void> {
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       from: "LmCars <noreplay@lmcars.com.br>",
       subject,
-      text: body,
-      html: body,
+      html: templateHTML,
     });
 
     console.log("Message sent", message.messageId);
